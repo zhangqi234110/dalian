69c9c10d0c8d269293f7f660bbc03b3e6bdc3104
zhangqi234110 <314890945@qq.com>
2020-03-01 12:49:04 +0800

    修改你好啊

---
+++ src/main/java/com/example/dalian/controller/GitController.java
@@ -0,0 +1,11 @@
+package com.example.dalian.controller;
+
+/**
+ * Created by 张齐 on 2020/3/1 11:21
+ */
+public class GitController {
+    public static void main(String[] args) {
+
+        System.out.println("你好啊");
+    }
+}
---
+++ src/main/java/com/example/dalian/controller/JiaoTongController.java
@@ -0,0 +1,12 @@
+package com.example.dalian.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 张齐 on 2020/2/28 10:56
+ */
+@RestController
+@RequestMapping("/transport")
+public class JiaoTongController {
+}
